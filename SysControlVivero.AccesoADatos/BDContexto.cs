a34c804055d231a1df17f61278d4c5de928fa5b0
Elsy Pineda <jaz_elsy@hotmail.com>
2023-03-21 19:19:43 -0600

    Agregar archivos de proyecto.

---
+++ SysControlVivero.AccesoADatos/BDContexto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using SysControlVivero.EntidadesDeNegocio;
+
+namespace SysControlVivero.AccesoADatos
+{
+    public class BDContexto : DbContext
+    {
+        public DbSet<Rol> Rol { get; set; }
+        public DbSet<Usuario> Usuario { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-DEV01\SQLEXPRESS02;Initial Catalog=SeguridadWebdb;Integrated Security=True");
+        }
+    }
+
+}
+
