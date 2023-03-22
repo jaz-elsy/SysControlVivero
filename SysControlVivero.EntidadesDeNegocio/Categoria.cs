9cd8481a0b85dc0cc1da3669fa06882ff68528ee
Carolina Hernández <carolina_2004her@hotmail.com>
2023-03-22 10:15:18 -0600

    Empleado y Categoria

---
+++ SysControlVivero.EntidadesDeNegocio/Categoria.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SysControlVivero.EntidadesDeNegocio
+{
+    public class Categoria
+    {
+        [Key]
+        public int IdCategoria { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "Maximo 50 caracteres")]
+        public string Nombre { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "Maximo 50 caracteres")]
+        public string Descripcion { get; set; }
+    }
+}
---
+++ SysControlVivero.EntidadesDeNegocio/Empleado.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SysControlVivero.EntidadesDeNegocio
+{
+    public class Empleado
+    {
+        [Key]
+        public int IdEmpleado { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "Maximo 50 caracteres")]
+        public string Nombre { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "Maximo 50 caracteres")]
+        public string Correo { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "Maximo 50 caracteres")]
+        public string DUI { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "Maximo 50 caracteres")]
+        public string Direccion { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "Maximo 50 caracteres")]
+        public string Cargo { get; set; }
+    }
+}
