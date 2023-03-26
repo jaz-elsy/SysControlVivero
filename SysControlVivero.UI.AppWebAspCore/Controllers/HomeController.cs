b4cdc21698240a2164707ac25042a861497f1eea
Elsy Pineda <jaz_elsy@hotmail.com>
2023-03-25 21:01:54 -0600

    Se creo la capa API

---
+++ SysControlVivero.UI.AppWebAspCore/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SysControlVivero.UI.AppWebAspCore.Models;
+using System.Diagnostics;
+
+namespace SysControlVivero.UI.AppWebAspCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
