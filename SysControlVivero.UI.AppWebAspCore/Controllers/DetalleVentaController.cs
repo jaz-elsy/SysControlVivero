d33547c732bcd13be7567515f63e4242a809cada
Alisson Abigail Cabrera Alcántara <Abigail12al@hotmail.com>
2023-03-26 21:32:13 -0600

    controlador factura programado

--- SysControlVivero.UI.AppWebAspCore/Controllers/DetalleVentaController.cs
+++ SysControlVivero.UI.AppWebAspCore/Controllers/DetalleVentaController.cs
@@ -1,83 +1,121 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+//****************
+using SysControlVivero.EntidadesDeNegocio;
+using SysControlVivero.LogicaDeNegocio;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using SysControlVivero.AccesoADatos;
 
 namespace SysControlVivero.UI.AppWebAspCore.Controllers
 {
+    [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme)]
     public class DetalleVentaController : Controller
     {
-        // GET: DetalleVentaController
-        public ActionResult Index()
+        
+        DetalleVentaBL _detalleventaBL = new DetalleVentaBL();
+
+        // GET: FacturaController
+        public async Task<IActionResult> Index(DetalleVenta pDetalleVenta = null)
         {
+            if (pDetalleVenta == null)
+                pDetalleVenta = new DetalleVenta();
+            if (pDetalleVenta.Top_Aux == 0)
+                pDetalleVenta.Top_Aux = 10;
+            else if (pDetalleVenta.Top_Aux == -1)
+                pDetalleVenta.Top_Aux = 0;
+            var detalleventas = await _detalleventaBL.BuscarAsync(pDetalleVenta);
+            ViewBag.Top = pDetalleVenta.Top_Aux;
+            return View(detalleventas);
+
+
+        }
+
+
+        // GET: FacturaController/Details/5
+        public async Task<ActionResult> Details(int id)
+        {
+            var detalleventa = await _detalleventaBL.ObtenerPorIdAsync(new DetalleVenta { IdDetalleVenta = id });
+            return View(detalleventa);
+        }
+
+
+        // GET: FacturaController/Create
+        public IActionResult Create()
+        {
+            ViewBag.Error = "";
             return View();
         }
 
-        // GET: DetalleVentaController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: DetalleVentaController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: DetalleVentaController/Create
+        // POST: FacturaController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+
+        public async Task<IActionResult> Create(DetalleVenta pDetalleVenta)
         {
             try
             {
+                int result = await _detalleventaBL.CrearAsync(pDetalleVenta);
                 return RedirectToAction(nameof(Index));
             }
-            catch
+            catch (Exception ex)
             {
-                return View();
+                ViewBag.Error = ex.Message;
+                return View(pDetalleVenta);
             }
         }
-
         // GET: DetalleVentaController/Edit/5
-        public ActionResult Edit(int id)
+        public async Task<IActionResult> Edit(DetalleVenta pDetalleVenta)
         {
-            return View();
+            var detalleventa = await _detalleventaBL.ObtenerPorIdAsync(pDetalleVenta);
+            ViewBag.Error = "";
+
+            return View(detalleventa);
         }
 
         // POST: DetalleVentaController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public async Task<IActionResult> Edit(int id, DetalleVenta pDetalleVenta)
         {
             try
             {
+                int result = await _detalleventaBL.ModificarAsync(pDetalleVenta);
                 return RedirectToAction(nameof(Index));
             }
-            catch
+            catch (Exception ex)
             {
-                return View();
+                ViewBag.Error = ex.Message;
+                return View(pDetalleVenta);
+
             }
         }
 
         // GET: DetalleVentaController/Delete/5
-        public ActionResult Delete(int id)
+        public async Task<IActionResult> Delete(DetalleVenta pDetalleVenta)
         {
-            return View();
+            ViewBag.Error = "";
+            var detalleventa = await _detalleventaBL.ObtenerPorIdAsync(pDetalleVenta);
+            return View(detalleventa);
         }
 
-        // POST: DetalleVentaController/Delete/5
+        // POST: FacturaController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public async Task<IActionResult> Delete(int id, DetalleVenta pDetalleVenta)
         {
             try
             {
+                int result = await _detalleventaBL.EliminarAsync(pDetalleVenta);
                 return RedirectToAction(nameof(Index));
             }
-            catch
+            catch (Exception ex)
             {
-                return View();
+                ViewBag.Error = ex.Message;
+                return View(pDetalleVenta);
             }
         }
     }
 }
+        
+
--- SysControlVivero.UI.AppWebAspCore/Controllers/FacturaController.cs
+++ SysControlVivero.UI.AppWebAspCore/Controllers/FacturaController.cs
@@ -1,83 +1,124 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SysControlVivero.EntidadesDeNegocio;
+using SysControlVivero.LogicaDeNegocio;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace SysControlVivero.UI.AppWebAspCore.Controllers
 {
+    [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme)]
     public class FacturaController : Controller
     {
+
+        FacturaBL _facturaBL = new FacturaBL();
+
         // GET: FacturaController
-        public ActionResult Index()
+        public async Task<IActionResult> Index(Factura pFactura = null)
         {
-            return View();
+            if (pFactura == null)
+                pFactura = new Factura();
+            if (pFactura.Top_Aux == 0)
+                pFactura.Top_Aux = 10;
+            else if (pFactura.Top_Aux == -1)
+                pFactura.Top_Aux = 0;
+            var facturas = await _facturaBL.BuscarAsync(pFactura);
+            ViewBag.Top = pFactura.Top_Aux;
+            return View(facturas);
+
+
         }
+
 
         // GET: FacturaController/Details/5
-        public ActionResult Details(int id)
+        public async Task<ActionResult> Details(int id)
         {
-            return View();
+            var factura = await _facturaBL.ObtenerPorIdAsync(new Factura { IdFactura = id });
+            return View(factura);
         }
 
+
         // GET: FacturaController/Create
-        public ActionResult Create()
+        public IActionResult Create()
         {
+            ViewBag.Error = "";
             return View();
         }
 
         // POST: FacturaController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+
+        public async Task<IActionResult> Create(Factura pFactura)
         {
             try
             {
+                int result = await _facturaBL.CrearAsync(pFactura);
                 return RedirectToAction(nameof(Index));
             }
-            catch
+            catch (Exception ex)
             {
-                return View();
+                ViewBag.Error = ex.Message;
+                return View(pFactura);
             }
         }
 
-        // GET: FacturaController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
 
+        // GET: FacturaController/Edit/5
+        public async Task<IActionResult> Edit(Factura pFactura)
+        {
+
+
+            var factura = await _facturaBL.ObtenerPorIdAsync(pFactura);
+            ViewBag.Error = "";
+
+            return View(factura);
+
+        }
         // POST: FacturaController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public async Task<IActionResult> Edit(int id, Factura pFactura)
         {
             try
             {
+                int result = await _facturaBL.ModificarAsync(pFactura);
                 return RedirectToAction(nameof(Index));
             }
-            catch
+            catch (Exception ex)
             {
-                return View();
+                ViewBag.Error = ex.Message;
+                return View(pFactura);
+
             }
         }
 
         // GET: FacturaController/Delete/5
-        public ActionResult Delete(int id)
+        public async Task<IActionResult> Delete(Factura pFactura)
         {
-            return View();
+            ViewBag.Error = "";
+            var factura = await _facturaBL.ObtenerPorIdAsync(pFactura);
+            return View(factura);
         }
-
+      
         // POST: FacturaController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public async Task<IActionResult> Delete(int id, Factura pFactura)
         {
             try
             {
+                int result = await _facturaBL.EliminarAsync(pFactura);
                 return RedirectToAction(nameof(Index));
             }
-            catch
+            catch (Exception ex)
             {
-                return View();
+                ViewBag.Error = ex.Message;
+                return View(pFactura);
             }
         }
     }
 }
+
+    
+
